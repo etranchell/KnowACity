d61cb219fbc3c035edaa2875843a89d564104002
eric <erictranchell@yahoo.com>
2023-03-23 17:24:11 -0400

    changes

--- JavaScript.js
+++ JavaScript.js
@@ -6,25 +6,10 @@ const options = {
 	}
 };
 
+
+
 fetch('https://weatherapi-com.p.rapidapi.com/current.json?q=East%20Lansing', options)
 	.then(response => response.json())
-	.then(response => console.log(response))
+	.then(response => console.log(response.current.cloud))
 	.catch(err => console.error(err));
 
-              
-        
-
-    fetch('https://maps.googleapis.com/maps/api/js?key=AIzaSyDgw_SCqiJ739w3UyCxOTGNQQx2OuiK6vs&callback=initMap', options)
-        .then(response => response.json())
-        .then(response => console.log(response))
-        .catch(err => console.error(err));
-        
-        var modules = google.maps.modules = {};
-        google.maps.__gjsload__ = function(name, text) {
-          modules[name] = text;
-        };
-
-        window.google = window.google || {};
-        google.maps = google.maps || {};
-        
-    
